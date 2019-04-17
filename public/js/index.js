931625831077a5659311a5439cb772b75705c71b
Arto Kir <artavazd1980@gmail.com>
2019-04-17 22:11:51 +0900

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -7,27 +7,6 @@ socket.on('disconnect', function () {
   console.log('dissconnected from server');
 });
 
-socket.on('newEmail', function (email) {
-  console.log('new email');
-  console.log(email);
+socket.on('newMessage', function (message) {
+console.log(message);
 });
-
-socket.emit('createEmail', {
-to: 'gane@email',
-text: 'hey from browser'
-});
-
-
-socket.emit('createMessage', {
-from: 'gane',
-text: 'hey from browser',
-createdAt: new Date()
-});
-
-
-socket.on('newMessage', function (newMessage) {
-console.log(newMessage);
-});
-
-//newMessage
-//createMessage
