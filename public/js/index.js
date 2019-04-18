b18fd58d394ecf808a992ec537d7af0ca2ace57d
Arto Kir <artavazd1980@gmail.com>
2019-04-19 00:29:57 +0900

    add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -12,13 +12,24 @@ socket.on('connection', function(message) {
   console.log(message);
 });
 
-socket.on('newMessage', function (message) {
+socket.on('newLocationMessage', function (message) {
 console.log(message);
 var li = jQuery('<li></li>');
-li.text(`${message.from}: ${message.text}`);
+li.text(`${message.from}:ssss ${message.text}`);
+
 jQuery('#messages').append(li);
 });
 
+
+socket.on('newLocationMessage', function(message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target = "_blank">My Current Location</a>')
+
+  li.text(`${message.from}: `)
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
 
 // socket.emit('createMessage', {
 //   from: 'Frenk',
@@ -36,4 +47,19 @@ socket.emit('createMessage', {
 }, function () {
 
 });
+});
+
+
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser')
+  };
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', { latitude: position.coords.latitude, longitude: position.coords.longitude});
+  }, function () {
+    alert('Unable to fetch location');
+  })
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('Generate Message', () => {
 
@@ -13,3 +13,21 @@ expect(typeof message.createdAt).toBe('number');
 expect(message).toMatchObject({from, text});
 });
 });
+
+
+
+
+describe('Generate Location Message', () => {
+  it('should generate correct location object', () => {
+
+var from = 'Admin';
+var latitude = 36.365126599999996;
+var longitude = 127.33958369999998;
+var url = `https://www.google.co.kr/maps?q=${latitude},${longitude}`;
+
+var location = generateLocationMessage(from, latitude, longitude);
+
+expect(location.from).toBe(from);
+expect(location.url).toBe(url);
+  })
+})
